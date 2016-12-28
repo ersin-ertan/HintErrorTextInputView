39084b3b5306da1688f28977d57082ff3f24c8eb
eersin <eersin@mmsofts.com>
2016-12-28 23:33:23 +0700

    Initial commit

---
+++ hinterrortextinputview/src/main/java/com/github/ersin_ertan/hinterrortextinputview/HintErrorTextInputView.java
@@ -0,0 +1,23 @@
+package com.github.ersin_ertan.hinterrortextinputview;
+
+import android.content.Context;
+import android.support.design.widget.TextInputLayout;
+import android.util.AttributeSet;
+
+/**
+ * Created by mms on 12/28/16.
+ */
+
+public class HintErrorTextInputView extends TextInputLayout {
+  public HintErrorTextInputView(Context context) {
+    super(context);
+  }
+
+  public HintErrorTextInputView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public HintErrorTextInputView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+}
